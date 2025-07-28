2aa4f164c48d2aa1b54c4eabd9d2e6202079fe03
juliocesardemoraes <jcmcf2012@gmail.com>
2025-07-27 23:22:51 -0300

    feat: add patch test

--- src/index.js
+++ src/index.js
@@ -15,7 +15,25 @@ app.get("/", (request, response) => {
   response.status(200).send({ working: true });
 });
 
-app.post("/users", (request, response) => {
+app.patch("/users", (request, response) => {
+  // Caso o request.body não tenha um id não insira nenhum usuário
+  // retorne o código 400 e a mensagem Id faltante
+
+  // Faça uma implementação baseada no usersMock
+  // Retorne o array de users
+  response.status(201).send({});
+});
+
+app.patch("/users/:id", (request, response) => {
+  // Caso o request.body não tenha um id não insira nenhum usuário
+  // retorne o código 400 e a mensagem Id faltante
+
+  // Faça uma implementação baseada no usersMock
+  // Retorne o array de users
+  response.status(200).send({});
+});
+
+app.put("/users", (request, response) => {
   // Caso o request.body não tenha um id não insira nenhum usuário
   // retorne o código 400 e a mensagem Id faltante
 
--- src/tests/app.test.js
+++ src/tests/app.test.js
@@ -2,9 +2,8 @@ import request from "supertest";
 import app from "../index.js";
 import { usersMock } from "../mocks/users.js";
 
-describe("POST /users", () => {
+describe("PATCH /users", () => {
   const mockUserToInsert = {
-    id: 6,
     name: "javier",
     email: "javier@gmail.com",
     birthdate: "1997-03-17",
@@ -20,22 +19,36 @@ describe("POST /users", () => {
     active: false,
   };
 
-  it("A rota deve existir e enviar o código 201 caso insira um user corretamente", async () => {
-    const response = await request(app).post(`/users`).send(mockUserToInsert);
-    expect(response.statusCode).toBe(201);
-  });
+  function getRandomInt(max, min = 0) {
+    const num = Math.floor(Math.random() * max);
+    num + min;
+    return num;
+  }
 
-  it("A rota deve retornar código 400 caso não exista id no body da request", async () => {
+  it("A rota deve existir", async () => {
+    const generatedId = getRandomInt(1000, 100);
+    mockUserToInsert.id = generatedId;
+
     const response = await request(app)
-      .post(`/users`)
-      .send(mockUserWithMissingIdToInsert);
-    expect(response.statusCode).toBe(400);
+      .patch(`/users/${mockUserToInsert.id}`)
+      .send(mockUserToInsert);
+
+    console.log("STATUS", response.statusCode);
+
+    expect(response.statusCode).not.toBe(404);
   });
 
-  it("Deve adicionar um novo usuario ao array de usersMock quando chamado", async () => {
-    const response = await request(app).post(`/users`).send(mockUserToInsert);
-    expect(response.statusCode).toBe(201);
+  // it("A rota deve retornar código 400 caso não exista id no body da request", async () => {
+  //   const response = await request(app)
+  //     .post(`/users`)
+  //     .send(mockUserWithMissingIdToInsert);
+  //   expect(response.statusCode).toBe(400);
+  // });
 
-    expect(usersMock).toContainEqual(mockUserToInsert);
-  });
+  // it("Deve adicionar um novo usuario ao array de usersMock quando chamado", async () => {
+  //   const response = await request(app).post(`/users`).send(mockUserToInsert);
+  //   expect(response.statusCode).toBe(201);
+
+  //   expect(usersMock).toContainEqual(mockUserToInsert);
+  // });
 });
